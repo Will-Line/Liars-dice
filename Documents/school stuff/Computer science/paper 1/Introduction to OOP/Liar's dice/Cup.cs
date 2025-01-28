5a0ea98c2e93d614a97eb698120fd0746720886a
William Line <william.line@proton.me>
2025-01-28 12:28:58 +0000

    Dice and Cup classes complete

--- Documents/school stuff/Computer science/paper 1/Introduction to OOP/Liar's dice/Cup.cs
+++ Documents/school stuff/Computer science/paper 1/Introduction to OOP/Liar's dice/Cup.cs
@@ -1,4 +1,17 @@
 class Cup
 {
-    
+    public Dice[] diceArray;
+
+    public Cup(int diceNumber)
+    {
+        diceArray=new Dice[diceNumber];
+    }
+
+    public void rollDie()
+    {
+        for(int i=0;i<diceArray.Length;i++)
+        {
+            diceArray[i].RollDice();
+        }
+    }
 }
--- Documents/school stuff/Computer science/paper 1/Introduction to OOP/Liar's dice/Dice.cs
+++ Documents/school stuff/Computer science/paper 1/Introduction to OOP/Liar's dice/Dice.cs
@@ -1,4 +1,12 @@
 class Dice
 {
-    
+    private int diceValue;
+    public int DiceValue{ get; }
+
+    public void RollDice()
+    {
+        Random random=new Random();
+
+        diceValue=random.Next(1,7);
+    }
 }
